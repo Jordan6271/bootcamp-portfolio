ac09d2cba4960105c43a7ea7b548c89596f068e9
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-06-01 19:52:11 +0100

    Delete currently unused css file and import

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Routes from "./routes/Routes";
