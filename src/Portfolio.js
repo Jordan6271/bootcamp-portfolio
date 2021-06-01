ec8e9dbf9eb9ab4970071a5735bb56b8f260153b
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-06-01 19:34:53 +0100

    Create navbar component

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Routes from "./routes/Routes";
 
 const Portfolio = () => {
     return (
         <Fragment>
             <Router>
+                <Navbar />
                 <Switch>
                     <Route component={Routes} />
                 </Switch>
---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from "react";
+
+const Navbar = () => {
+    return <Fragment>Navbar</Fragment>;
+};
+
+export default Navbar;
