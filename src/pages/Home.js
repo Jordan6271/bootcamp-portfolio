aed917b399f0b7d7aadc9501a430a03b6e0764da
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-06-08 20:11:46 +0100

    Add content to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,82 @@
 import React, { Fragment } from "react";
 
 const Home = () => {
-    return <Fragment>Homepage to my portfolio!</Fragment>;
+    return (
+        <Fragment>
+            <div id="about-me">
+                <h2>About Me</h2>
+                <p>
+                    My name is Jordan, and I have always been interested in
+                    programming in general. I've tried my hand at creating
+                    websites, making lots of interesting tools, and designing
+                    simple games. This has given me some experience using HTML,
+                    CSS, JavaScript, and C++. This portfolio is specifically
+                    going to be regarding the projects I've been working on
+                    throughout my time on a bootcamp programming course taught
+                    by The Developer Academy. I will be using this design as a
+                    base to make a portfolio of my personal projects in the
+                    future.
+                </p>
+            </div>
+            <div id="current-work">
+                <h2>My Current Work</h2>
+                <p>
+                    I currently work with Mattress Online as an apprentice on
+                    the web team. My main tasks recently have been associated
+                    with writing automated tests using Cypress, which has a
+                    JavaScript framework, and a lot of the projects I've worked
+                    on so far with this bootcamp has helped me in my everyday
+                    work, and I've been able to see significant progress in how
+                    I am thinking about and writing my tests. It's also given me
+                    a great basis for getting up to speed with the working life
+                    of a developer, and how teamwork and communication can be so
+                    important in such an environment.
+                </p>
+            </div>
+            <div id="past-experience">
+                <h2>Past Experience</h2>
+                <p>
+                    Other than my current work, all of my IT and coding
+                    experience has really come from personal projects and
+                    playing around with things as a hobby. I did Astrophysics at
+                    university, and while I did do a couple of programming
+                    modules, the only other part of my degree that is relevant
+                    to me nowadays is the mathematical parts, which definitely
+                    helps with game design. After university I had a
+                    non-technical entry-level job, and spent a lot of my spare
+                    time trying to make websites and simple video games (along
+                    with playing lots of video games, which has given me a large
+                    amount of passion to eventually make a 'proper' game that
+                    I'd enjoy sitting down and playing for as long as I have
+                    sunk into some others).
+                </p>
+            </div>
+            <div id="portfolio-info">
+                <h2>About This Portfolio</h2>
+                <p>
+                    I've decided the best way for me to lay this portfolio out
+                    is likely to be giving each project its own individual page,
+                    and describing there how I tackled each task and screenshots
+                    of what I ended up with, along with any hurdles I
+                    encountered on the journey and how they were overcome. There
+                    will also be a link to each of the project repositories on
+                    GitHub, and the GitHub pages link for you to check out how
+                    things function for yourselves. I hope you enjoy my work.
+                </p>
+                <p>
+                    Also, as a note, I will mention that this portfolio is not
+                    going to be hosted on a personal domain because I would like
+                    to create my own portfolio after the bootcamp is finished
+                    using this as a template, and I don't want to spend too much
+                    time finding non-copyrighted images and animations for all
+                    of the projects I've done so far. If any projects are to be
+                    included in my personal portfolio that is hosted elsewhere,
+                    rest assured that I will do my due diligence and make sure
+                    none of the content is stolen or copyrighted.
+                </p>
+            </div>
+        </Fragment>
+    );
 };
 
 export default Home;
