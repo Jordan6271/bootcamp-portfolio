ddb0f99b483aacb100b0b4c0d9720602fabd816c
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-06-08 20:06:10 +0100

    Remove unused css file

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
-import "./Carousel.css";
 
 const MyCarousel = (props) => {
     const [images, setImages] = useState([]);
