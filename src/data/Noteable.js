e054c8e860f2fef4a24fd890ef68ffc6a5283977
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-06-17 17:43:26 +0100

    Add content to noteable

--- src/data/Noteable.js
+++ src/data/Noteable.js
@@ -2,11 +2,11 @@ import Images from "../images/week10/Images";
 
 const images = Images;
 
-const introText = ``;
+const introText = `This project essentially was to make a social site, with cards for new posts and a form for a user to add a new post. It gave me an introduction to proper state management with React, and I got my first taste of pulling props through from one component to another, and using it there. There is also some error handling added on the form to create a new post.`;
 
-const difficultyText = ``;
+const difficultyText = `I had some ideas about this project to allow users to further customise their notes by changing the colour. This gave some issues in the sense that in order to do this, I found a free image of a sticky note, and recoloured it, to have 6 different options for users. When this is posted to the board, it isn't particularly responsive, and gets cut off at smaller screen sizes. This caused me a lot of aggro, but I decided to just keep it as it is in the end.`;
 
-const progressionText = ``;
+const progressionText = `This project helped me grasp the concept of passing props through to child components, which proving to be incredibly useful for upcoming projects. It also gave me an understanding of how to add items and objects to the users' localStorage, and manipulate the information shown to them through that.`;
 
 const links = {
     ghPages: `https://jordan6271.github.io/bootcamp-project-week10/#/`,
