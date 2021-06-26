f5d1c1f889e49ea65f7d51eee03378dc1a9cbb35
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-06-26 15:01:13 +0100

    Try hash router

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Routes from "./routes/Routes";
@@ -14,7 +14,7 @@ const Portfolio = () => {
                     <Navbar />
                     <Switch>
                         <div className="w-75 justify-content-center m-auto row text-light text-center pt-4">
-                            <Route exact path="/" component={Home} />
+                            <Route exact path="/#/" component={Home} />
                             <Route component={Routes} />
                         </div>
                     </Switch>
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -6,21 +6,29 @@ const Routes = () => {
     return (
         <section>
             <Switch>
-                <Route exact path="/week1" component={Sitemap.BasicWebPage} />
-                <Route exact path="/week2" component={Sitemap.HobbyWebsite} />
-                <Route exact path="/week4" component={Sitemap.JavaScriptQuiz} />
+                <Route exact path="/week1#/" component={Sitemap.BasicWebPage} />
+                <Route exact path="/week2#/" component={Sitemap.HobbyWebsite} />
                 <Route
                     exact
-                    path="/week5"
+                    path="/week4#/"
+                    component={Sitemap.JavaScriptQuiz}
+                />
+                <Route
+                    exact
+                    path="/week5#/"
                     component={Sitemap.OOPAdventureGame}
                 />
-                <Route exact path="/week7" component={Sitemap.CaesarCipher} />
-                <Route exact path="/week9" component={Sitemap.Calculator} />
-                <Route exact path="/week10" component={Sitemap.Noteable} />
-                <Route exact path="/week11" component={Sitemap.WhatToDo} />
-                <Route exact path="/week12" component={Sitemap.WeatherWatch} />
-                <Route exact path="/week13" component={Sitemap.BookListAPI} />
-                <Route exact path="/week14" component={Sitemap.Eventful} />
+                <Route exact path="/week7#/" component={Sitemap.CaesarCipher} />
+                <Route exact path="/week9#/" component={Sitemap.Calculator} />
+                <Route exact path="/week10#/" component={Sitemap.Noteable} />
+                <Route exact path="/week11#/" component={Sitemap.WhatToDo} />
+                <Route
+                    exact
+                    path="/week12#/"
+                    component={Sitemap.WeatherWatch}
+                />
+                <Route exact path="/week13#/" component={Sitemap.BookListAPI} />
+                <Route exact path="/week14#/" component={Sitemap.Eventful} />
                 <Route component={Sitemap.NotFound} />
             </Switch>
         </section>
